008a637996bcd1a3f08bbe45979b25219d0c17c3
Chloe <chloepeng331@icloud.com>
2022-02-22 21:20:28 -0500

    commit tree 67d6096c5879988f4bd5f2600f8cd7bbb78ad4f4

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Home from "./Home";
+// import About from "./About";
+// import Home from "./Home";
+
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>
+  )
 }
 
 export default NavBar;
+
+{/* <a href ={About}/> will not work because react does know what {About} is */}
